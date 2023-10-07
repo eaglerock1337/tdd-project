6ea8dc9c84a245c71dc2d03b9b33374de16f4616
Peter Marks <pmarks@signiant.com>
2023-10-07 18:13:54 -0400

    test 2

--- py/test_money.py
+++ py/test_money.py
@@ -7,6 +7,13 @@ class Dollar:
   def times(self, multiplier):
     return Dollar(self.amount * multiplier)
 
+class Money:
+  def __init__(self, amount, currency):
+    self.amount = amount
+    self.currency = currency
+  
+  def times(self, multiplier):
+    return Money(self.amount * multiplier, self.currency)
 
 class TestMoney(unittest.TestCase):
   def testMultiplication(self):
@@ -14,6 +21,11 @@ class TestMoney(unittest.TestCase):
     tenner = fiver.times(2)
     self.assertEqual(10, tenner.amount)
 
+  def testMultiplicationInEuros(self):
+    tenEuros = Money(10, "EUR")
+    twentyEuros = tenEuros.times(2)
+    self.assertEqual(20, twentyEuros.amount)
+    self.assertEqual("EUR", twentyEuros.currency)
 
 if __name__ == '__main__':
   unittest.main()
